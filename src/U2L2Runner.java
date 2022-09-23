c78a445b779116f78fe3b709f76223ef57bed07d
BT_4N2_26 <sanan6576@bths.edu>
2022-09-23 12:33:09 -0400

    copy and pasted assignment

--- src/U2L2Runner.java
+++ src/U2L2Runner.java
@@ -1,2 +1,21 @@
 public class U2L2Runner {
+   public static void main(String[] args) {
+      Rectangle backyard1 = new Rectangle(150, 200);
+      Rectangle backyard2 = new Rectangle(100);
+      Rectangle backyard3 = new Rectangle();
+
+      backyard1.setWidth(125);
+      backyard2.setWidth(125);
+      backyard2.setLength(125);
+      backyard3.setWidth(125);
+
+      int perimeter;
+      
+   }
+
+
+
+
+
+
 }
