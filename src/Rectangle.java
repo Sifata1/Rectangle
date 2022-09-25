2a4741d46f3174f781069bb07658d96707637da2
Sifat <sanan6576@bths.edu>
2022-09-25 18:08:30 -0400

    added method to calculate area and perimeter.

--- src/Rectangle.java
+++ src/Rectangle.java
@@ -76,4 +76,12 @@ public class Rectangle
         System.out.println("This rectangle's length is " + length + " and width is " + width + ".");
         System.out.println("It has an area of " + calculateArea());
     }
+
+    public int calculatePerimeter()
+    {
+        int perimeter = 2 * (width + length);
+        return perimeter;
+    }
+
+
 }
--- src/U2L2Runner.java
+++ src/U2L2Runner.java
@@ -1,5 +1,6 @@
 public class U2L2Runner {
-   public static void main(String[] args) {
+   public static void main(String[] args)
+   {
       Rectangle backyard1 = new Rectangle(150, 200); // These are the set lengths of the plots.
       Rectangle backyard2 = new Rectangle(100);
       Rectangle backyard3 = new Rectangle();
@@ -9,16 +10,15 @@ public class U2L2Runner {
       backyard2.setLength(125);
       backyard3.setWidth(125);
 
-      int backyard1Perimeter = ((backyard1.getLength() + backyard1.getWidth())*2); // This calculates the perimeters of each of the plots.
-      int backyard2Perimeter = ((backyard2.getLength() + backyard2.getWidth())*2);
-      int backyard3Perimeter = ((backyard3.getLength() + backyard3.getWidth())*2);
-
-      int perimeter = (backyard1Perimeter) + (backyard2Perimeter) + (backyard3Perimeter); // This calculates the areas of the plots.
-      System.out.println("The total length of fencing that will be needed is " + perimeter);
+      int perimeter = (backyard1.calculatePerimeter()) + (backyard2.calculatePerimeter()) + (backyard3.calculatePerimeter());
+      System.out.println("The total area that will need seeding is " + perimeter);
 
       int area = (backyard1.calculateArea()) + (backyard2.calculateArea()) + (backyard3.calculateArea());
       System.out.println("The total area that will need seeding is " + area);
-      
+
    }
 
-}
+
+   }
+
+
