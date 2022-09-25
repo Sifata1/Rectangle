fa5b9d74953236b5cc2a493cd1cd212a65aecc24
sifatanan <sanan6576@bths.edu>
2022-09-25 08:27:10 -0400

    added method to calculate area and perimeter.

--- src/U2L2Runner.java
+++ src/U2L2Runner.java
@@ -9,13 +9,16 @@ public class U2L2Runner {
       backyard2.setLength(125);
       backyard3.setWidth(125);
 
-      int perimeter;
+      int backyard1Perimeter = ((backyard1.getLength() + backyard1.getWidth())*2);
+      int backyard2Perimeter = ((backyard2.getLength() + backyard2.getWidth())*2);
+      int backyard3Perimeter = ((backyard3.getLength() + backyard3.getWidth())*2);
+
+      int perimeter = (backyard1Perimeter) + (backyard2Perimeter) + (backyard3Perimeter);
+      System.out.println("The total length of fencing that will be needed is " + perimeter);
+
+      int area = (backyard1.calculateArea()) + (backyard2.calculateArea()) + (backyard3.calculateArea());
+      System.out.println("The total area that will need seeding is " + area);
       
    }
-
-
-
-
-
 
 }
