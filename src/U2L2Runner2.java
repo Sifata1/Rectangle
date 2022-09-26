5072251d1ec856e079bb3ddaab4fe5eefffce056
Sifat <sanan6576@bths.edu>
2022-09-25 20:06:28 -0400

    added method to calculate area and perimeter.

--- src/U2L2Runner2.java
+++ src/U2L2Runner2.java
@@ -4,16 +4,14 @@ public class U2L2Runner2 {
     public static void main(String[] args) {
         Scanner s = new Scanner(System.in);
         System.out.println("How many pieces of lumber do you need for the home you are building?");
-        int numOfPiecesOfLumber = s.nextInt();
+        int numOfPiecesOfLumber = s.nextInt(); // The number of the pieces of lumber is inputted by the user.
         System.out.println("How many windows do you need?");
-        int numOfWindows = s.nextInt();
+        int numOfWindows = s.nextInt(); // The number of windows is inputted by the user.
         System.out.println("What is the tax rate for your area?");
-        double taxRate = s.nextDouble();
+        double taxRate = s.nextDouble(); // The tax rate is inputted by the user.
 
         ConstructionPricer prices = new ConstructionPricer(11.5, 25.75, taxRate);
-        double cost = prices.materialsCost(numOfPiecesOfLumber, numOfWindows);
-        cost = Math.round(cost * 100);
-        cost = (cost/100);
+        double cost = prices.materialsCost(numOfPiecesOfLumber, numOfWindows); // The total cost is calculated.
         double total = prices.totalWithTax(cost);
 
         System.out.println("Total Materials Cost: $" + cost);
